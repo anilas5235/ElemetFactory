0f2195abc63e2499881d9ac12cd0d70903b44db8
anilas5235 <niklas-borchers@outlook.de>
2023-08-09 20:26:13 +0200

    Voice System

--- Assets/Project/AudioSynthesis/Synthesizer.cs
+++ Assets/Project/AudioSynthesis/Synthesizer.cs
@@ -1,88 +1,104 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Project.AudioSynthesis
 {
-    [RequireComponent(typeof( AudioSource))]
+    [RequireComponent(typeof(AudioSource))]
     public class Synthesizer : MonoBehaviour
     {
-        public enum WaveTypes
+        public enum WaveType
         {
             Sin,
             Square,
+            Saw,
             Triangle,
-            RufSin,
+            Noise,
         }
 
-        [Range(220, 1760)] public double frequency = 440;
-        [Range(0, 1f)] public float amplitude = 0.1f;
+        public WaveType waveType;
 
-        public WaveTypes waveType;
+        [Range(0, 1f)] public float gain = 0.5f;
 
-        public bool frequencyShifter;
-        public Vector2 range = new Vector2(440,880);
-        public float shiftingSpeed = 10;
+        private const int polyphony = 16;
+        private Voice[] voicesPool;
 
-        private int _bufferLength,_numBuffers, _sampleRate;
+        private Stack<Voice> freeVoices;
+        private List<Voice> activeVoices;
+        private Dictionary<int, Voice> noteDict;
 
-        private double _phase, _increment;
-        
-        private void OnAudioFilterRead(float[] buffer, int channels)
-        {
-            _increment = frequency/_sampleRate;
-
-            for (int sample = 0; sample < buffer.Length; sample += channels)
-            {
-                _phase = (_phase + _increment) % 1;
-                
-                float t = (float) _phase *2* Mathf.PI;
-
-                float sampleValue = 0;
-
-                switch (waveType)
-                {
-                    case WaveTypes.Sin:sampleValue = Mathf.Sin(t);
-                        break;
-                    case WaveTypes.Square:sampleValue = Mathf.Sin(t) >= 0 ? 1 : -1;
-                        break;
-                    case WaveTypes.Triangle:sampleValue = Mathf.PingPong(t, 2) - 1;
-                        break;
-                    case WaveTypes.RufSin: sampleValue = Mathf.Sin(t + .1f* Mathf.Cos(t));
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                sampleValue *= amplitude;
-                buffer[sample] = buffer[sample + 1] = sampleValue;
-            }
-        }
-
-        private void Start()
-        {
-
-            AudioSettings.GetDSPBufferSize(out _bufferLength, out _numBuffers);
-            _sampleRate = AudioSettings.outputSampleRate;
-
-            print($"sampleRate = {_sampleRate} | bufferLength = {_bufferLength} | numBuffers = {_numBuffers}");
-            
-        }
 
         private void OnEnable()
         {
-            if(frequencyShifter) StartCoroutine(FrequencyShifter(range.x, range.y,shiftingSpeed));
-        }
+            NoteInput.NoteOn += NoteInput_NoteOn;
+            NoteInput.NoteOff += NoteInput_NoteOff;
 
-        private IEnumerator FrequencyShifter(float minRange, float maxRange, float speed)
-        {
-            do
+            voicesPool = new Voice[polyphony];
+            freeVoices = new Stack<Voice>();
+
+            for (int i = 0; i < voicesPool.Length; i++)
             {
-                frequency+=speed;
-                if (frequency > maxRange) frequency = minRange;
-                yield return new WaitForFixedUpdate();
-            } while (true);
+                voicesPool[i] = new Voice();
+                freeVoices.Push(voicesPool[i]);
+            }
+
+            activeVoices = new List<Voice>();
+            noteDict = new Dictionary<int, Voice>();
         }
 
+        private void OnDisable()
+        {
+            NoteInput.NoteOn -= NoteInput_NoteOn;
+            NoteInput.NoteOff -= NoteInput_NoteOff;
+        }
+
+        public void NoteInput_NoteOn(int notenumber, float velocity)
+        {
+            if (noteDict.ContainsKey(notenumber)) return;
+            if (freeVoices.Count <= 0) return;
+
+            Voice voice = freeVoices.Pop();
+            voice.waveType = waveType;
+            voice.NoteOn(notenumber, velocity);
+
+            activeVoices.Add(voice);
+
+            noteDict.Add(notenumber, voice);
+        }
+
+        public void NoteInput_NoteOff(int notenumber)
+        {
+            if (!noteDict.ContainsKey(notenumber)) return;
+
+            Voice voice = noteDict[notenumber];
+            voice.NoteOff(notenumber,this);
+        }
+
+        public void VoiceFinishedPlaying(Voice voice)
+        {
+             activeVoices.Remove(voice);
+             freeVoices.Push(voice);
+            
+            noteDict.Remove(voice.noteNumber);
+        }
+
+        private void OnAudioFilterRead(float[] buffer, int channels)
+        {
+            for (int i = 0; i < activeVoices.Count; i++)
+            {
+                activeVoices[i].WriteAudioBuffer(buffer, channels);
+            }
+
+            for (int i = 0; i < buffer.Length; i += channels)
+            {
+                buffer[i] *= gain;
+
+                for (int c = 1; c < channels; c++)
+                {
+                    buffer[i + c] = buffer[i];
+                }
+            }
+        }
     }
 }
